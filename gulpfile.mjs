5442a53cadbb823a739df1ae3099efa5389ae581
Mark Tanasevych <mark.tanasevych@academysmart.com>
2024-01-24 02:06:38 +0200

    update

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -75,6 +75,8 @@ const config = {
 }
 var plugins = [
   './node_modules/jquery/dist/jquery.js',
+  './node_modules/@popperjs/core/dist/umd/popper.js',
+  './node_modules/bootstrap/dist/js/bootstrap.js'
 ];
 
 const list_plugins = {
